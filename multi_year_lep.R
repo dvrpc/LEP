dad4c72f1cb15754a4aaae07227ed71d988127d2
jDobkin <78820023+jDobkin@users.noreply.github.com>
2023-09-20 13:32:44 -0400

    Update multi_year_lep.R

--- multi_year_lep.R
+++ multi_year_lep.R
@@ -1,16 +1,16 @@
-getwd()
+years <- 2016:2020
 
-vars2020 <- load_variables(2020, "acs5")
+# Set your working directory
+setwd("your\path\here")
 
 #census keys
-census_api_key("", overwrite = TRUE)
-ckey <- ""
+Sys.getenv("CENSUS_API_KEY")
+census_api_key("CENSUS_API_KEY", overwrite = TRUE)
+ckey <- "CENSUS_API_KEY"
 
 # Loop through each year
-for (year in years) {
-  vars <- load_variables(year, "acs5")
+for (lep_year in years) {
   
-  # Get data for pumas
   pumas <- get_acs(geography = "public use microdata area",
                    variables = c16001, 
                    survey = "acs5", 
@@ -20,7 +20,6 @@ for (year in years) {
                    geometry = FALSE,
                    cache = TRUE)
   
-  # Get data for tracts
   tracts <- get_acs(geography = "tract",
                     variables = c16001, 
                     survey = "acs5", 
@@ -30,7 +29,6 @@ for (year in years) {
                     geometry = FALSE,
                     cache = TRUE)
   
-  # Get data for pumas2
   pumas2 <- get_acs(geography = "public use microdata area",
                     variables = b16001, 
                     survey = "acs5", 
@@ -40,40 +38,26 @@ for (year in years) {
                     geometry = FALSE,
                     cache = TRUE)
   
-  # Filtering
+  # Filter data
   tracts <- filter(tracts, as.numeric(substr(tracts$GEOID, start = 1, stop = 5)) %in% lep_counties)
   pumas <- filter(pumas, pumas$GEOID %in% puma_Codes)
   pumas2 <- filter(pumas2, pumas2$GEOID %in% puma_Codes)
   
-  # Joining data
-  pumas3 <- left_join(pumas, pumas2, by = "GEOID")
+  # Create a mapping of oldnames to newnames
+  col_mapping <- setNames(newnames, oldnames)
   
-  # Renaming columns
-  oldnames <- c("old_column_names") # Replace with actual column names
-  newnames <- c("new_column_names") # Replace with desired new column names
+  # Rename columns based on the mapping
+  pumas <- pumas %>%
+    rename_with(~ ifelse(. %in% oldnames, col_mapping[.], .), .cols = everything())
   
-  tracts <- tracts %>% rename_at(vars(oldnames), ~ newnames)
-  pumas <- pumas %>% rename_at(vars(oldnames), ~ newnames)
-  pumas2 <- pumas2 %>% rename_at(vars(oldnames), ~ newnames)
+  tracts <- tracts %>%
+    rename_with(~ ifelse(. %in% oldnames, col_mapping[.], .), .cols = everything())
   
-  # Adding calculations
-  tracts$pop5pct_E <- (.05 * tracts$TT_POP_E)
-  pumas$pop5pct_E <- (.05 * pumas$TT_POP_E)
-  
-  tracts$TT_POP_LEP_E <- with(tracts, sum(Span_Lim_E, FRE_Lim_E, GER_Lim_E, RUS_Lim_E, IND_Lim_E, KOR_Lim_E,
-                                          CHI_Lim_E, Viet_Lim_E, TAG_Lim_E, PAC_Li_E, ARB_Lim_E, OTH_Lim_E))
-  pumas$TT_POP_LEP_E <- with(pumas, sum(Span_Lim_E, FRE_Lim_E, GER_Lim_E, RUS_Lim_E, IND_Lim_E, KOR_Lim_E,
-                                        CHI_Lim_E, Viet_Lim_E, TAG_Lim_E, PAC_Li_E, ARB_Lim_E, OTH_Lim_E))
+  pumas2 <- pumas2 %>%
+    rename_with(~ ifelse(. %in% oldnames, col_mapping[.], .), .cols = everything())
   
   # Export data
-  write_csv(pumas, here(paste0("pumasLEP_", year, ".csv")))
-  write_csv(tracts, here(paste0("tractsLEP_", year, ".csv")))
-  write_csv(pumas2, here(paste0("longGrainLEP_", year, ".csv")))
-  write_csv(vars, here(paste0("lep", year, "_vars.csv")))
-  
-  # Export shapefiles
-  st_write(pumas2, here(paste0("longGrain_", year, ".shp")))
-  st_write(pumas, here(paste0("pumas_", year, ".shp")))
-  st_write(tracts, here(paste0("tracts_", year, ".shp")))
+  write_csv(pumas, here(paste0("pumasLEP_", lep_year, ".csv")))
+  write_csv(tracts, here(paste0("tractsLEP_", lep_year, ".csv")))
+  write_csv(pumas2, here(paste0("longGrainLEP_", lep_year, ".csv")))
 }
-
